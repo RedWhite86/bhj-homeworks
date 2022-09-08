346898ecab1937bf6dde9549237075ff90d205bb
RedWhite86 <martynov-f@yandex.ru>
2022-09-08 14:46:09 +0300

    slider

---
+++ element-search/slider/task.js
@@ -0,0 +1,30 @@
+const rightArrow = document.querySelector('.slider__arrow_next');
+const leftArrow = document.querySelector('.slider__arrow_prev');
+const itemsArr = Array.from(document.getElementsByClassName('slider__item'));
+
+let activeItemIndex = itemsArr.findIndex((link) => link.className = 'slider__item slider__item_active');
+
+rightArrow.onclick = showNextItem;
+leftArrow.onclick = showPreviousItem;
+
+function showNextItem() {
+  itemsArr[activeItemIndex].className = 'slider__item';
+  if (activeItemIndex === itemsArr.length - 1) {
+    activeItemIndex = 0;
+    itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
+  } else {
+    activeItemIndex++;
+    itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
+  }
+}
+
+function showPreviousItem() {
+  itemsArr[activeItemIndex].className = 'slider__item';
+  if (activeItemIndex === 0) {
+    activeItemIndex = itemsArr.length - 1;
+    itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
+  } else {
+    activeItemIndex--;
+    itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
+  }
+}
