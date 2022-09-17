246b8e237f355499dca242e30eca724eb86e927b
RedWhite86 <martynov-f@yandex.ru>
2022-09-17 18:31:09 +0300

    Задача 1

---
+++ dom/reveal/task.js
@@ -0,0 +1,15 @@
+const revealArray = Array.from(document.getElementsByClassName('reveal'));
+document.addEventListener('scroll', checkPosition);
+
+function checkPosition() {
+  revealArray.forEach(element => {
+    const {bottom, top} = element.getBoundingClientRect();
+    if (bottom < 0) {
+      return element.classList.remove('reveal_active');
+    }
+    if (top > window.innerHeight) {
+      return element.classList.remove('reveal_active');
+    }
+    return element.classList.add('reveal_active');
+  });
+};
